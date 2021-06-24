2a28b370c9b5fe32314f695e00b27efbba61848c
Ney Brito <ney.brito@etec.sp.gov.br>
2021-06-24 16:25:40 -0300

    Developed list all map

--- src/main/java/one/digitalinnovatio/personapijava/service/PersonService.java
+++ src/main/java/one/digitalinnovatio/personapijava/service/PersonService.java
@@ -8,6 +8,10 @@ import one.digitalinnovatio.personapijava.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.mapstruct.Mapper;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
@@ -30,5 +34,11 @@ public class PersonService {
                 .builder()
                 .message("Created person with ID " + savedPerson.getId())
                 .build();
+    }
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
